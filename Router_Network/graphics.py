5785c404aefbc5549748cfffa9ae5470d407b635
Tom <tomacunningham@gmail.com>
2023-12-23 22:38:07 -0600

    Adding seasons

--- Router_Network/graphics.py
+++ Router_Network/graphics.py
@@ -1,9 +1,34 @@
 import pygame
+import numpy as np
+from enum import Enum
+
+HEIGHT = 500
+WIDTH = 500
+REDRAW = 25
+
+# Define some colors
+darkgrey = (40, 40, 40)
+lightgrey = (150, 150, 150)
+lightbrown = (202,176,0)
+darkbrown = (128,89,0)
+blue = (0,0,255)
+
+field = np.zeros((WIDTH,HEIGHT))
+
+class Season(Enum):
+    Tillage = 1
+    Planting = 2
+    Grow1 = 3
+    Grow2 = 4
+    Grow3 = 5
+    FullGrown = 6
+    Golden = 7
+    Harvest = 8
 
 class graphics:
     def __init__(self):
         pygame.init()
-
+        self.season = Season.Tillage
         pygame.display.set_caption("Multi Rover Control")
         # Set the width and height of the screen [width,height]
         size = [500, 500]
@@ -33,3 +58,12 @@ class graphics:
                 key_press = event.key
         
         return run_loop, key_press
+    
+    # Given two points,determine which pixels to color
+    def DetermineWhichPixelsToColor(x1,y1,x2,y2,field):
+        dx = x2 - x1
+        dy = y2 - y1
+        distance  = x2-x1
+        for x in range(int(distance)):
+            y = y1 + dy * (x - x1) / dx
+            field[int(x1),int(y)] = 1
